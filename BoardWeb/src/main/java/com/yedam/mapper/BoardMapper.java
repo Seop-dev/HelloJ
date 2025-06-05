6cdd5b499c8b66153bf44afeed3ee0c79193de5d
Seop-dev <Devseop@gmail.com>
2025-06-05 17:37:30 +0900

    Board Web

---
+++ BoardWeb/src/main/java/com/yedam/mapper/BoardMapper.java
@@ -0,0 +1,17 @@
+package com.yedam.mapper;
+
+import java.util.List;
+
+import com.yedam.vo.BoardVO;
+
+/*
+ * 인터페이스(BoardMapper) - XML(BoardMapper.xml) 매칭
+ */
+public interface BoardMapper {
+	public List<BoardVO> selectList(); // 목록
+	public BoardVO selectBoard(int bno); // 단건 조회
+	public int updateReadCnt(int bno); // 조회 count 증가
+	public int insertBoard(BoardVO board);
+	public int updateBoard(BoardVO board);
+	public int deleteBoard(int bno);
+}
